d71589e7abca325965214c1cf64fcf2ba73d33d6
HTFI physics <physics.htfi@gmail.com>
2026-02-24 07:52:54 +0900

    feat: useScrollフックを新規作成し、Imageコンポーネントのスクロールロジックを分離

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,39 +1,24 @@
 import { Chip, Stack } from "@mui/material";
 import { useSelectedItemNodeValue } from "../../jotai/useSelectedTreeNode";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { getBase64 } from "./getBase64";
+import { useScroll } from "./useScroll";
 
 export function Image() {
   const selectedItem: ItemNode | null = useSelectedItemNodeValue();
   const [nodeId, setNodeId] = useState<string>();
   const [svg, setSvg] = useState<string | null>(null);
-  const ref = useRef<HTMLImageElement | null>(null);
-  const [time, setTime] = useState<number>(0);
-  const [scrolling, setScrolling] = useState<boolean>(true);
-  const scrollTime = 240 * 1000;
-
-  useEffect(() => {
-    if (!svg || !ref.current || !scrolling) return;
-    const scrollSpeed = ref.current.clientHeight / scrollTime; // スクロール量（px/ms）
-    const interval = 1 / scrollSpeed; // 1pxスクロールする時間（ms）
-    const delay =
-      (0.5 * scrollTime * window.innerHeight) / ref.current.clientHeight;
-    const id = setInterval(() => {
-      setTime((t) => t + interval);
-      if (delay < time) {
-        window.scrollBy(0, scrollSpeed * interval);
-      }
-    }, interval);
-    return () => clearInterval(id);
-  }, [svg, time, scrolling, scrollTime]);
+  const { ref, scrolling, toggleScroll, timerStart } = useScroll(
+    "normal",
+    !!svg,
+  );
 
   const handle = selectedItem?.parent?.handle ?? null;
   const fileName = selectedItem ? selectedItem.data.title + ".svg" : null;
   if (!selectedItem || !handle || !fileName) return null;
 
   if (nodeId !== selectedItem.nodeId) {
-    setTime(0);
-    setScrolling(true);
+    timerStart();
     setNodeId(selectedItem.nodeId);
     getBase64(handle, fileName)
       .then(setSvg)
@@ -42,13 +27,7 @@ export function Image() {
 
   if (!svg) return null;
   return (
-    <Stack
-      direction="row"
-      onClick={() => {
-        setScrolling((s) => !s);
-        setTime(scrollTime);
-      }}
-    >
+    <Stack direction="row" onClick={toggleScroll}>
       <img ref={ref} src={"data:image/svg+xml;base64," + svg} />
       <Chip
         label="自動スクロール"
---
+++ src/components/Image/useScroll.ts
@@ -0,0 +1,32 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useScroll(speed: "normal" | "fast" | "slow", hasSvg: boolean) {
+  const ref = useRef<HTMLImageElement | null>(null);
+  const [time, setTime] = useState<number>(0);
+  const [scrolling, setScrolling] = useState<boolean>(true);
+  const scrollTime = { normal: 240, fast: 180, slow: 300 }[speed] * 1000;
+
+  useEffect(() => {
+    if (!hasSvg || !ref.current || !scrolling) return;
+    const ms_p_px = scrollTime / ref.current.clientHeight; // 1pxスクロールするのに要する時間（ms）
+    const delay =
+      (0.5 * scrollTime * window.innerHeight) / ref.current.clientHeight; // 画面の半分をスクロールするのにかかる時間（ms）
+    const id = setInterval(() => {
+      setTime((t) => t + ms_p_px);
+      if (delay < time) window.scrollBy(0, 1); // 1pxずつスクロールする
+    }, ms_p_px);
+    return () => clearInterval(id);
+  }, [hasSvg, time, scrolling, scrollTime]);
+
+  const timerStart = useCallback(() => {
+    setTime(0);
+    setScrolling(true);
+  }, []);
+
+  const toggleScroll = useCallback(() => {
+    setScrolling((s) => !s);
+    setTime(scrollTime);
+  }, [scrollTime]);
+
+  return { ref, toggleScroll, timerStart, scrolling };
+}
