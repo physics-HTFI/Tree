94a7f3dd22c5d032784257e8e92a554d857ba633
HTFI physics <physics.htfi@gmail.com>
2026-02-24 20:42:42 +0900

    feat: TreeViewコンポーネントにティアの自動開閉機能を追加し、useHiddenTiersフックを統合

--- src/components/TreeView/TreeView.tsx
+++ src/components/TreeView/TreeView.tsx
@@ -2,11 +2,33 @@ import { RichTreeView } from "@mui/x-tree-view/RichTreeView";
 import { CustomTreeViewItem } from "./TreeViewItem";
 import { useFilteredTreeItemsValue } from "../../jotai/useTreeItems";
 import { useSelectedTreeNodeId } from "../../jotai/useSelectedTreeNode";
+import { useState } from "react";
+import { useHiddenTiersValue } from "../../jotai/useHiddenTiers";
 
 export function TreeView() {
   // フック
+  const hiddenTiers = useHiddenTiersValue();
   const tree = useFilteredTreeItemsValue();
   const [selectedItemId, setSelectedItemId] = useSelectedTreeNodeId();
+  const [expandedIds, setExpandedIds] = useState<string[]>([]);
+  const [showsTier0, setShowsTier0] = useState(true);
+
+  // ティア変更時の自動開閉
+  if (showsTier0 === hiddenTiers.has(0)) {
+    setShowsTier0(!showsTier0);
+    if (showsTier0) {
+      const getIds = (subTree: TreeNode | null, ids: string[]) => {
+        if (subTree?.type === "folder") {
+          ids.push(subTree.nodeId);
+          subTree.children.forEach((child) => getIds(child, ids));
+        }
+        return ids;
+      };
+      setExpandedIds(getIds(tree, []));
+    } else {
+      setExpandedIds([]);
+    }
+  }
 
   // イベントハンドラー
   const getItemId = (item: TreeNode) => item.nodeId;
@@ -29,6 +51,8 @@ export function TreeView() {
       getItemLabel={getItemLabel}
       isItemSelectionDisabled={isItemSelectionDisabled}
       onItemSelectionToggle={onItemSelectionToggle}
+      expandedItems={expandedIds}
+      onExpandedItemsChange={(_event, ids) => setExpandedIds(ids)}
       slots={{ item: CustomTreeViewItem }}
       sx={{
         p: 1,
--- src/jotai/useHiddenTiers.ts
+++ src/jotai/useHiddenTiers.ts
@@ -1,4 +1,5 @@
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { _atomHiddenTiers } from "./share/_atomHiddenTiers";
 
 export const useHiddenTiers = () => useAtom(_atomHiddenTiers);
+export const useHiddenTiersValue = () => useAtomValue(_atomHiddenTiers);
