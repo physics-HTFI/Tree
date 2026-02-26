1c98b84608d090e0b63137009feb00f7ae2f1bca
HTFI physics <physics.htfi@gmail.com>
2026-02-26 12:50:49 +0900

    feat: filterTree関数を外部に移動し、hiddenTiersを引数として受け取るように変更

--- src/jotai/share/atomFilteredTree.ts
+++ src/jotai/share/atomFilteredTree.ts
@@ -6,21 +6,20 @@ export const atomFilteredTreeValue = atom<FolderNode | null>((get) => {
   const tree = structuredClone(get(_atomTreeItems));
   const hiddenTiers = get(atomHiddenTiers);
   if (!tree) return null;
-
-  const filterTree = (items: FolderNode): FolderNode => {
-    const children: TreeNode[] = [];
-    for (const item of items.children) {
-      if (item.type === "folder") {
-        const filteredChild = filterTree(item);
-        if (filteredChild.children.length === 0 && hiddenTiers.has(0)) continue;
-        children.push(filteredChild);
-      } else {
-        if (hiddenTiers.has(item.entry.tier ?? 0)) continue; // チェックが外れているティアは表示しない
-        children.push(item);
-      }
-    }
-    return { ...items, children };
-  };
-
-  return filterTree(tree);
+  return filterTree(tree, hiddenTiers);
 });
+
+function filterTree(items: FolderNode, hiddenTiers: Set<number>): FolderNode {
+  const children: TreeNode[] = [];
+  for (const item of items.children) {
+    if (item.type === "folder") {
+      const filteredChild = filterTree(item, hiddenTiers);
+      if (filteredChild.children.length === 0 && hiddenTiers.has(0)) continue;
+      children.push(filteredChild);
+    } else {
+      if (hiddenTiers.has(item.entry.tier ?? 0)) continue; // チェックが外れているティアは表示しない
+      children.push(item);
+    }
+  }
+  return { ...items, children };
+}
