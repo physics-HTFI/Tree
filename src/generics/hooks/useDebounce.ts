8f004f6f0664f4776bc54a7a82e958abb464d239
HTFI physics <physics.htfi@gmail.com>
2026-02-28 16:04:59 +0900

    useDebounceフックにconsume関数を追加し、即時実行機能を実装しました。また、内部のタイマー管理をrefを使用して改善しました。

--- src/generics/hooks/useDebounce.ts
+++ src/generics/hooks/useDebounce.ts
@@ -3,23 +3,37 @@ import { useCallback, useRef } from "react";
 export function useDebounce<T>(fn: (arg: T) => void): {
   debounced: (arg: T, delay: number) => void;
   cancel: () => void;
+  consume: () => void;
 } {
-  const timerRef = useRef<ReturnType<typeof setTimeout>>(0);
+  const ref = useRef<{ time: ReturnType<typeof setTimeout>; args: T } | null>(
+    null,
+  );
 
   const cancel = useCallback(() => {
-    if (timerRef.current !== 0) {
-      clearTimeout(timerRef.current);
-      timerRef.current = 0;
-    }
+    if (!ref.current) return;
+    clearTimeout(ref.current?.time);
+    ref.current = null;
   }, []);
 
   const debounced = useCallback(
     (args: T, delay: number) => {
       cancel();
-      timerRef.current = setTimeout(() => fn(args), delay);
+      ref.current = {
+        time: setTimeout(() => {
+          fn(args);
+          cancel();
+        }, delay),
+        args,
+      };
     },
     [cancel, fn],
   );
 
-  return { debounced, cancel };
+  const consume = useCallback(() => {
+    if (!ref.current) return;
+    fn(ref.current.args); // 即時実行
+    cancel();
+  }, [fn, cancel]);
+
+  return { debounced, cancel, consume };
 }
