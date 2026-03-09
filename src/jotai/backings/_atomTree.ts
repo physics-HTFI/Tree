19744dfe14282945da8e2aefb3c9df8f955fd888
HTFI physics <physics.htfi@gmail.com>
2026-03-09 18:19:27 +0900

    fullTreeの定義を別の定数atomFullTreeに移動し、コードの可読性を向上させました。

--- src/jotai/backings/_atomTree.ts
+++ src/jotai/backings/_atomTree.ts
@@ -3,18 +3,18 @@ import { atom } from "jotai";
 const atomDataTree = atom<FolderNode>();
 const atomReferenceTree = atom<FolderNode>();
 
+const atomFullTree = atom((get) => {
+  const dataTree = get(atomDataTree);
+  const referenceTree = get(atomReferenceTree);
+  if (!dataTree || !referenceTree) return undefined;
+  return {
+    ...dataTree,
+    children: [...dataTree.children, referenceTree],
+  };
+});
+
 export const _atomTree = {
   dataTree: atomDataTree,
-
   referenceTree: atomReferenceTree,
-
-  fullTree: atom((get) => {
-    const dataTree = get(atomDataTree);
-    const referenceTree = get(atomReferenceTree);
-    if (!dataTree || !referenceTree) return undefined;
-    return {
-      ...dataTree,
-      children: [...dataTree.children, referenceTree],
-    };
-  }),
+  fullTree: atomFullTree,
 };
