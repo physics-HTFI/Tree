5e15e1af4045a99c5ad68895047582f5c2bc6d06
HTFI physics <physics.htfi@gmail.com>
2026-03-09 18:08:30 +0900

    _appSettingsを削除し、atomAppSettingsValueを非同期関数に変更してフォルダから設定を読み込むように修正しました。また、setFoldersAsync内で設定を取得するようにしました。

--- src/jotai/atomAppSettings.ts
+++ src/jotai/atomAppSettings.ts
@@ -1,4 +1,10 @@
 import { atom } from "jotai";
-import { _atomAppSettings } from "./backings/_atomAppSettings";
+import { appFileSystem } from "./utils/appFileSystem";
+import { _atomFolders } from "./backings/_atomFolders";
 
-export const atomAppSettingsValue = atom((get) => get(_atomAppSettings));
+export const atomAppSettingsValue = atom(async (get) => {
+  const folders = get(_atomFolders);
+  const settings = await appFileSystem.readAppSettingsAsync(folders?.data);
+  if (!settings) return {};
+  return settings;
+});
--- src/jotai/atomFolders.ts
+++ src/jotai/atomFolders.ts
@@ -1,16 +1,16 @@
 import { atom } from "jotai";
 import { appFileSystem } from "./utils/appFileSystem";
-import { _atomAppSettings } from "./backings/_atomAppSettings";
 import { _atomTree } from "./backings/_atomTree";
 import { createTreeItems } from "./utils/createTreeItems";
 import { _atomReferenceData } from "./backings/_atomReferenceData";
 import { _atomFolders } from "./backings/_atomFolders";
 import { setFaviconSvg } from "./utils/setFaviconSvg";
+import { atomAppSettingsValue } from "./atomAppSettings";
 
 const setFoldersAsync = atom(
   null,
   async (
-    _,
+    get,
     set,
     folders: {
       data: FileSystemDirectoryHandle;
@@ -21,11 +21,6 @@ const setFoldersAsync = atom(
     await folders.reference.requestPermission();
     set(_atomFolders, folders);
 
-    // Settingsを読み込んでatomにセットする
-    const settings = await appFileSystem.readAppSettingsAsync(folders.data);
-    if (!settings) return;
-    set(_atomAppSettings, settings);
-
     // ReferenceDataを読み込んでatomにセットする
     const referenceData = await appFileSystem.readReferenceDataAsync(
       folders.data,
@@ -33,7 +28,7 @@ const setFoldersAsync = atom(
     if (referenceData) set(_atomReferenceData, referenceData);
 
     // フォルダからTreeItemsを生成してatomにセットする
-    if (!settings?.tiers) return;
+    const settings = await get(atomAppSettingsValue);
     set(
       _atomTree.dataTree,
       await createTreeItems.fromDataFolder(folders.data, settings.ignore),
