ae9ba66fa921a10ae957abe90c29ecfced643265
HTFI physics <physics.htfi@gmail.com>
2026-03-10 21:07:02 +0900

    validateFolderNodeおよびvalidateItemNodeのロジックを整理し、関数を明確に分離しました。また、isUrl関数のインポート名を変更しました。

--- src/models/validators/validateFolderNode.ts
+++ src/models/validators/validateFolderNode.ts
@@ -1,36 +1,40 @@
 export const validateFolderNode = {
-  canAddFolder: (
-    folder: NewFolderNode,
-    parent?: FolderNode,
-  ): parent is FolderNode => {
-    if (!parent) return false;
-
-    // title
-    if (folder.title === "") return false;
-    const duplicated = parent.children?.some(
-      (child) =>
-        child.type === "folder" &&
-        child.title.toLowerCase() === folder.title.toLowerCase(),
-    );
-    if (duplicated) return false;
-
-    // path
-    if (folder.path === "") return false; // 空文字は許可しない（未指定の場合はundefinedにする）
-
-    return true;
-  },
-
-  modifyNewFolder: (folder: NewFolderNode) => {
-    folder.title = folder.title.trim();
-    folder.path = folder.path?.trim();
-    if (folder.path === "") folder.path = undefined;
-  },
-
-  canRemoveChild: (nodeId: string, children: TreeNode[]) => {
-    const target = children.find((child) => child.nodeId === nodeId);
-    if (!target) return false;
-    if (target.type === "folder") return false;
-    if (target.hasSvg) return false;
-    return true;
-  },
+  canAddFolder,
+  modifyNewFolder,
+  canRemoveChild,
 };
+
+function canAddFolder(
+  folder: NewFolderNode,
+  parent?: FolderNode,
+): parent is FolderNode {
+  if (!parent) return false;
+
+  // title
+  if (folder.title === "") return false;
+  const duplicated = parent.children?.some(
+    (child) =>
+      child.type === "folder" &&
+      child.title.toLowerCase() === folder.title.toLowerCase(),
+  );
+  if (duplicated) return false;
+
+  // path
+  if (folder.path === "") return false; // 空文字は許可しない（未指定の場合はundefinedにする）
+
+  return true;
+}
+
+function modifyNewFolder(folder: NewFolderNode) {
+  folder.title = folder.title.trim();
+  folder.path = folder.path?.trim();
+  if (folder.path === "") folder.path = undefined;
+}
+
+function canRemoveChild(nodeId: string, children: TreeNode[]) {
+  const target = children.find((child) => child.nodeId === nodeId);
+  if (!target) return false;
+  if (target.type === "folder") return false;
+  if (target.hasSvg) return false;
+  return true;
+}
--- src/models/validators/validateItemNode.ts
+++ src/models/validators/validateItemNode.ts
@@ -1,48 +1,53 @@
-import { isUrl } from "@/generics/utils/isUrl";
+import { isUrl as _isUrl } from "@/generics/utils/isUrl";
 
 export const validateItemNode = {
-  isValidItem: (item: ItemEntry) => {
-    if (item.title === undefined) return false;
-    return true;
-  },
-
-  canAddItem: (item?: ItemEntry, parent?: FolderNode) => {
-    if (!item?.title || !parent) return false;
-    if (!validateItemNode.isValidItem(item)) return false;
-    const duplicated = parent.children?.some(
-      (c) =>
-        c.type === "item" &&
-        c.entry.title?.toLowerCase() === item.title?.toLowerCase(),
-    );
-    if (duplicated) return false;
-    return true;
-  },
-
-  canOverwrite: (item: ItemEntry, node: ItemNode) => {
-    if (item.title === node.entry.title) return true;
-    const duplicated = node.parent?.children?.some(
-      (c) =>
-        c.type === "item" &&
-        c.entry.title?.toLowerCase() === item.title?.toLowerCase() &&
-        c.nodeId !== node.nodeId,
-    );
-    if (duplicated) return false;
-    return true;
-  },
-
-  isUrl: (item: ItemEntry) => {
-    return isUrl(item.path);
-  },
-
-  modifyItemNode: (item: ItemEntry) => {
-    if (!item.title) item.title = "";
-    if (!item.path) item.path = undefined;
-    if (!item.notes) item.notes = undefined;
-    if (item.tier === 0) item.tier = undefined;
-    if (!item.start || item.start <= 0) item.start = undefined;
-    if (!item.ticks || item.ticks <= 0) item.ticks = undefined;
-    if (!item.highlighted) item.highlighted = undefined;
-    if (!item.window) item.window = undefined;
-    if (isUrl(item.path)) item.window = undefined;
-  },
+  canAddItem,
+  canOverwrite,
+  isUrl,
+  modifyItemNode,
 };
+
+function isValidItem(item: ItemEntry) {
+  if (item.title === undefined) return false;
+  return true;
+}
+
+function canAddItem(item?: ItemEntry, parent?: FolderNode) {
+  if (!item?.title || !parent) return false;
+  if (!isValidItem(item)) return false;
+  const duplicated = parent.children?.some(
+    (c) =>
+      c.type === "item" &&
+      c.entry.title?.toLowerCase() === item.title?.toLowerCase(),
+  );
+  if (duplicated) return false;
+  return true;
+}
+
+function canOverwrite(item: ItemEntry, node: ItemNode) {
+  if (item.title === node.entry.title) return true;
+  const duplicated = node.parent?.children?.some(
+    (c) =>
+      c.type === "item" &&
+      c.entry.title?.toLowerCase() === item.title?.toLowerCase() &&
+      c.nodeId !== node.nodeId,
+  );
+  if (duplicated) return false;
+  return true;
+}
+
+function isUrl(item: ItemEntry) {
+  return _isUrl(item.path);
+}
+
+function modifyItemNode(item: ItemEntry) {
+  if (!item.title) item.title = "";
+  if (!item.path) item.path = undefined;
+  if (!item.notes) item.notes = undefined;
+  if (item.tier === 0) item.tier = undefined;
+  if (!item.start || item.start <= 0) item.start = undefined;
+  if (!item.ticks || item.ticks <= 0) item.ticks = undefined;
+  if (!item.highlighted) item.highlighted = undefined;
+  if (!item.window) item.window = undefined;
+  if (_isUrl(item.path)) item.window = undefined;
+}
