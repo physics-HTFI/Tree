ac9c2bc09a2ef7b58bb874e019e880a1d74e5e69
HTFI physics <physics.htfi@gmail.com>
2026-03-11 17:18:24 +0900

    isUrl関数の実装を簡素化し、URLの検証をHTTPおよびHTTPSプロトコルのチェックからHTTPSプロトコルのチェックに変更しました。

--- src/generics/utils/isUrl.ts
+++ src/generics/utils/isUrl.ts
@@ -1,9 +1,4 @@
 export function isUrl(path: string | undefined): boolean {
   if (!path) return false;
-  try {
-    const url = new URL(path);
-    return url.protocol === "http:" || url.protocol === "https:";
-  } catch {
-    return false;
-  }
+  return path.startsWith("https://");
 }
